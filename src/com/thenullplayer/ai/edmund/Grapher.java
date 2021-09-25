9b03e8cb77f73d3e938a95996377ffa1bcb90212
N <nathanmunn21@gmail.com>
2021-09-25 00:08:20 -0300

    Session and Grapher
    Session with (N.M.),
    Together with some added code in grapher class.

--- src/com/thenullplayer/ai/edmund/Grapher.java
+++ src/com/thenullplayer/ai/edmund/Grapher.java
@@ -1,16 +1,76 @@
 package com.thenullplayer.ai.edmund;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class Grapher
 {
-    public static void graph(){
-        String path = System.getProperty("user.dir") + File.separator + "data";
-        File data = new File(path);
-        File[] files = data.listFiles();
-        for (File file : files)
+    public static void graph()
+    {
+
+        File fileGraph = new File(System.getProperty("user.dir") + File.separator + "graph.dot");
+        try
         {
-            System.out.println(file);
+
+            System.out.println("Grapher: starting");
+
+            if(fileGraph.isFile())
+            {
+                fileGraph.delete();
+                fileGraph.createNewFile();
+            }
+
+            FileWriter graphFileWriter = new FileWriter(fileGraph, false);
+            PrintWriter graphPrintWriter = new PrintWriter(graphFileWriter);
+
+            //start the graph
+            graphPrintWriter.println("digraph {");
+
+            String path = System.getProperty("user.dir") + File.separator + "data";
+            File data = new File(path);
+            File[] files = data.listFiles();
+            for(File file : files)
+            {
+                //System.out.println(file);
+                try
+                {
+                    if(file.isFile())
+                    {
+                        String fileName = file.getName();
+                        String query = fileName.substring(0,fileName.length()-4);
+
+                        System.out.println("Grapher: \"" + query + "\" starting");
+
+                        Scanner fileReader = new Scanner(file);
+
+                        while(fileReader.hasNextLine())
+                        {
+                            graphPrintWriter.println("\"" + query + "\" -> \"" + fileReader.nextLine() + "\";");
+                        }
+
+                        fileReader.close();
+
+                        System.out.println("Grapher: \"" + query + "\" done");
+
+                    }
+
+                }
+                catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+            graphPrintWriter.println("}");
+
+            graphPrintWriter.close();
+
+            System.out.println("Grapher: done");
+
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
         }
     }
 }
